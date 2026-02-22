db7774d115649f9784b6f355928520e7e4cbd4cc
himalayo <himalayo@protonmail.com>
2026-02-22 18:23:03 +0000

    add benchmark for kernel profiling

---
+++ benchmark.cu
@@ -0,0 +1,76 @@
+//
+// Created by hima on 2/22/26.
+//
+
+#include "raycast.cuh"
+#include <nvtx3/nvToolsExt.h>
+
+int main()
+{
+    int worldWidth = 24, worldHeight = 24;
+    float posX = 22, posY = 12;  //x and y start position
+    float dirX = -1, dirY = 0; //initial direction vector
+    float planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+    int width = 1920;
+    int height = 600;
+    auto lines = static_cast<float *>(malloc(sizeof(float) * width));
+
+    nvtxRangePush("Initialization");
+    float* lines_gpu;
+    cudaMalloc(&lines_gpu, sizeof(float) * width);
+    int worldMap[24][24] =
+    {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
+
+    const int* map[worldHeight];
+    int* mapGpu;
+    cudaMalloc(&mapGpu, sizeof(int)*worldWidth*worldHeight);
+    nvtxRangePush("Map copying");
+    for (int i = 0; i < worldHeight; i++) {
+        map[i] = worldMap[i];
+        cudaMemcpy(mapGpu+(i*worldWidth), map[i], worldWidth * sizeof(int), cudaMemcpyHostToDevice);
+    }
+    nvtxRangePop();
+    nvtxRangePop();
+    int max_ray_length = (int)(sqrt(worldWidth*worldWidth + worldHeight*worldHeight)) + 1;
+    printf("%d %d\n", width, max_ray_length);
+    nvtxRangePush("Raycasting");
+    rayCastGpu<<<width, max_ray_length + 1>>>(lines_gpu, posX, posY, dirX, dirY, planeX, planeY, width, mapGpu, worldWidth, worldHeight, height/2.0f);
+    cudaDeviceSynchronize();
+    nvtxRangePop();
+    nvtxRangePush("Fetching results");
+    cudaMemcpy(lines, lines_gpu, width*sizeof(float), cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < width; i++) {
+        printf("%.4f ", lines[i]);
+    }
+    printf("\n");
+
+    nvtxRangePop();
+    cudaFree(lines_gpu);
+    cudaFree(mapGpu);
+}
